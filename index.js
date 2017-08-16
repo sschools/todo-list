78be9c273212ee8250d9cd80534ca36d3aa04e68
Stephen Schools <stephen.schools>
2017-08-16 14:26:33 -0400

    initial commit, list will add items, need to add complete button

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const mustacheExpress = require("mustache-express");
+
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/views");
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+
+const todos = [
+  {name: "Wash the car", complete: false}
+];
+
+app.get("/", function (request, respond) {
+  respond.render("index", { todos: todos });
+});
+
+app.post("/", function (request, respond) {
+  todos.push({"name":request.body.todo, "complete": false});
+  respond.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("Successfully started to do list.");
+});
